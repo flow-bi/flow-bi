a6af3ccab0a4e45c93bf45e2fc8fe50a993d058e
youm <juliet0524@naver.com>
2026-07-02 16:33:42 +0900

    chore: 백엔드 초기 세팅

---
+++ backend/src/main/java/com/flowbi/FlowbiApplication.java
@@ -0,0 +1,13 @@
+package com.flowbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowbiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlowbiApplication.class,args);
+  }
+
+}
