33886a343675d4d49700a30166b22f8b2e5851d2
bosung <qweasdzxc0311@naver.com>
2026-08-11 01:33:07 +0900

    ✨ / #38 - 초기 비밀번호 변경 강제 기능 구현

---
+++ backend/src/main/java/com/flowbi/domain/auth/password/PasswordChangeException.java
@@ -0,0 +1,8 @@
+package com.flowbi.domain.auth.password;
+
+public class PasswordChangeException extends RuntimeException {
+
+  public PasswordChangeException(String message) {
+    super(message);
+  }
+}
